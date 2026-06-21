20ce7e0c4ab72600fcc65c7ad611722054cecf12
mlang95 <mattlang95@gmail.com>
2026-06-21 16:35:04 -0400

    v0.4.9.1: wiki + reference pages, Mill merge, Cipher Chamber

--- Combatv3/build_all.bat
+++ Combatv3/build_all.bat
@@ -34,8 +34,8 @@ set BUILD_BOARD=1
 REM BOARD_DIR   : folder holding mapgen.py / hexmap.py / hexgen.py / build_board.py
 set BOARD_DIR=C:\Users\Matt\OneDrive\Desktop\Game\Combatv3\mapgen
 REM board dimensions, table size, seed, hex size (mm c->corner), paper
-set BOARD_W=14
-set BOARD_H=8
+set BOARD_W=13
+set BOARD_H=7
 set BOARD_PLAYERS=2
 set BOARD_SEED=96
 set BOARD_HEX=20
